39fecd378426736dd4e0d1c3d43667bd34c71ae6
tv0924@icloud.com <j93es@naver.com>
2023-07-19 22:09:15 +0900

    2023.07.19_0____update___major_motor.c___sub_*.c_*.h

---
+++ backup/backup3/*/Debug/External Library/Src/custom_switch.d
@@ -0,0 +1,26 @@
+External Library/Src/custom_switch.o: \
+ ../External\ Library/Src/custom_switch.c \
+ /Users/j93es/STM32CubeIDE/workspace_1.13.0/Nucleo-Linetracer/External\ Library/Inc/custom_switch.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_gpio.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/j93es/STM32CubeIDE/workspace_1.13.0/Nucleo-Linetracer/External\ Library/Inc/custom_delay.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_tim.h
+/Users/j93es/STM32CubeIDE/workspace_1.13.0/Nucleo-Linetracer/External\ Library/Inc/custom_switch.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_gpio.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/Users/j93es/STM32CubeIDE/workspace_1.13.0/Nucleo-Linetracer/External\ Library/Inc/custom_delay.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_tim.h:
