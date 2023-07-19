d2410d8fab36e658699299af8feae9f7f8b84b89
tv0924@icloud.com <j93es@naver.com>
2023-07-20 00:18:11 +0900

    2023.07.19_1____update___major__decision_machine___sub__*.c__*.h

--- Nucleo-Linetracer/Main/Src/decision_machine.c
+++ Nucleo-Linetracer/Main/Src/decision_machine.c
@@ -31,34 +31,47 @@ __STATIC_INLINE uint8_t Get_Marker_Sensor_Cnt() {
 
 // end line, right mark, left mark, cross를 판별하고 정해진 동작을 실행하는 함수
 __STATIC_INLINE void Decision(uint8_t sensorStateSum) {
+
 	// cross
-	if (~sensorStateSum == 0x00) {
+	// 0x7e == 0b01111110
+	// 라인 센서가 모두 인식됐을 경우 cross로 판별
+	if ( (sensorStateSum & 0x7e) == 0x7e ) {
 
 	}
+
 	// end line
 	else if ( ((sensorStateSum >> 0) & 0x01) && ((sensorStateSum >> 7) & 0x01) ) {
+		// end mark를 한번 이상 읽은 상태일 경우
 		if (endMarkCnt != 0) {
 			curDecisionState = DESISION_END_MARK;
 		}
+		// end mark를 한번도 안 읽은 상태일 경우
 		else {
 			curDecisionState = DESISION_STRAIGHT;
 			endMarkCnt++;
 		}
 	}
+
 	// right mark
 	else if ( (sensorStateSum >> 0) & 0x01 ) {
+		// 이전 마크가 오른쪽 곡선 마크였다면 곡선주행 종료
 		if (curDecisionState == DESISION_CURVE_R) {
 			curDecisionState = DESISION_STRAIGHT;
 		}
+		// 곡선주행 진입
 		else {
 			curDecisionState = DESISION_CURVE_R;
 		}
 	}
+
 	// left mark
-	else if ( (sensorStateSum >> 7) & 0x01 ) {
+	// else if ( (sensorStateSum >> 7) & 0x01 )
+	else {
+		// 이전 마크가 왼쪽 곡선 마크였다면 곡선주행 종료
 		if (curDecisionState == DESISION_CURVE_L) {
 			curDecisionState = DESISION_STRAIGHT;
 		}
+		// 곡선주행 진입
 		else {
 			curDecisionState = DESISION_CURVE_L;
 		}
@@ -77,17 +90,13 @@ void Decision_Machine() {
 	sensorStateSum = 0x00;
 
 	// 라인 센서 4개 이상 인식
-	// 0x7e == 0b01111110
 	if (Get_Line_Sensor_Cnt() >= 4) {
 
 		// 라인 센서가 읽은 값의 개수 3이하 일 때 센서값 누적 종료
 		while (Get_Line_Sensor_Cnt() >= 4) {
 			sensorStateSum |= state;
 		}
-		// cross 상태가 아닐 때만 decision
-		if (sensorStateSum != 0x7e) {
-			Decision(sensorStateSum);
-		}
+		Decision(sensorStateSum);
 	}
 
 	// 라인 센서 3개 이하 and 마크 센서 1개 이상
