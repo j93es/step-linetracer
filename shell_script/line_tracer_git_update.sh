c7cdd8ea8af94bc8048254bae4edc4ec995b8084
tv0924@icloud.com <j93es@naver.com>
2023-07-24 22:59:39 +0900

    2023.07.24_1____update____gitignore

--- shell_script/line_tracer_git_update.sh
+++
@@ -1,7 +0,0 @@
-rm -rf	~/Desktop/workspace/c/line-tracer/backup/backup5/*								&&
-mv	~/Desktop/workspace/c/line-tracer/backup/backup4/*	~/Desktop/workspace/c/line-tracer/backup/backup5/*	&&
-mv      ~/Desktop/workspace/c/line-tracer/backup/backup3/*      ~/Desktop/workspace/c/line-tracer/backup/backup4/*      &&
-mv      ~/Desktop/workspace/c/line-tracer/backup/backup2/*      ~/Desktop/workspace/c/line-tracer/backup/backup3/*      &&
-mv      ~/Desktop/workspace/c/line-tracer/backup/backup1/*      ~/Desktop/workspace/c/line-tracer/backup/backup2/* 	&&
-mv	~/Desktop/workspace/c/line-tracer/Nucleo-Linetracer	~/Desktop/workspace/c/line-tracer/backup/backup1/	&&
-cp -r	~/STM32CubeIDE/workspace_1.13.0/Nucleo-Linetracer	~/Desktop/workspace/c/line-tracer
